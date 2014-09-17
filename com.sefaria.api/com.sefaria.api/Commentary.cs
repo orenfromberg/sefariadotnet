c8c8d6e1b53a5df74c9eb11eb0bb5b1e85e041d7
Oren Fromberg <oren.fromberg@apx-labs.com>
2014-09-17 13:15:57 -0400

    adding JSON lib

---
+++ com.sefaria.api/com.sefaria.api/Commentary.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace com.sefaria.api
+{
+    public class Commentary
+    {
+        /// <summary>
+        /// The map id.
+        /// </summary>
+        [JsonProperty("id")]
+        public String Id { get; set; }
+
+        [JsonProperty("anchorRef")]
+        public String AnchorRef { get; set; }
+
+        
+    }
+}
