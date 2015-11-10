f32e623c45ff63ea7698ecb931d37469c74ed2c0
Oren Fromberg <oren.fromberg@apx-labs.com>
2015-11-09 22:41:41 -0500

    cleaning up, adding LICENSE.txt

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # sefariadotnet
 
-Small framework for sefaria api.
+Small framework for Sefaria API, with code heavily based on [stravadotnet](https://github.com/inexcitus/stravadotnet) by Sascha Simon, released under GPL v3.
 
 Please send feedback to orenfromberg@gmail.com
--- com.sefaria.api/com.sefaria.api/Http/WebRequest.cs
+++ com.sefaria.api/com.sefaria.api/Http/WebRequest.cs
@@ -1,40 +1,4 @@
-﻿#region Copyright (C) 2014 Sascha Simon
-
-//  This program is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation, either version 3 of the License, or
-//  (at your option) any later version.
-//
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU General Public License for more details.
-//
-//  You should have received a copy of the GNU General Public License
-//  along with this program.  If not, see http://www.gnu.org/licenses/.
-//
-//  Visit the official homepage at http://www.sascha-simon.com
-
-#endregion
-
-#region Copyright (C) 2015 Oren Fromberg
-
-//  This program is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation, either version 3 of the License, or
-//  (at your option) any later version.
-//
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU General Public License for more details.
-//
-//  You should have received a copy of the GNU General Public License
-//  along with this program.  If not, see http://www.gnu.org/licenses/.
-
-#endregion
-
-using System;
+﻿using System;
 using System.IO;
 using System.Net;
 using System.Net.Http;
